49d3a90d735eab1f94d3f496876e6a189d05bcb9
Jack He <jack.he@exciteholidays.com>
2015-09-17 17:34:27 +1000

    222

--- tests/Controller/AppTest.php
+++ tests/Controller/AppTest.php
@@ -5,6 +5,7 @@ class AppTest extends \PHPUnit_Framework_TestCase
 {
     public function test_app()
     {
-
+        $request = $this->getMockBuilder('FlickrSearch\Http\Request')->getMock();
+        $app = $this->getMockBuilder('FlickrSearch\Controller\App')->getMock();
     }
 }
